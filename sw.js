b9da0791d84ed543bce667c2831f71cbfbdabd64
MariusCiudin192 <24156112@ardenuniversity.ac.uk>
2025-09-07 14:16:31 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'vehicle-check-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'form.html',
+  'style.css',
+  'script.js',
+  'icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
